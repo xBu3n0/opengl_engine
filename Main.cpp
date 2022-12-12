5f1c047cb166ab79eafdb7e76bffa0769a292a59
Bueno <matheusbartkev@gmail.com>
2022-12-12 11:40:01 -0300

    1.0

--- Main.cpp
+++ Main.cpp
@@ -4,23 +4,17 @@
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
-#include <cstdint>
 #include <chrono>
-#include <cstdio>
-#include <ostream>
 #include <vector>
 #include <iostream>
 
 #include <thread>
-#include <utility>
 
 namespace windowName
 {
     const int MAIN = 0;
     const int SECONDARY = 1;
 }
-
-const int WIDTH = 600, HEIGHT = 400;
 
 
 void handleInput(std::vector<window::Window*>& windows)
@@ -30,6 +24,8 @@ void handleInput(std::vector<window::Window*>& windows)
     while(!windows.empty())
     {
         std::cout << windows.size() << std::endl;
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(100ms);
     }
 
     return;
@@ -53,11 +49,9 @@ int main()
     window::addWindow(windows);
     window::addWindow(windows);
 
-    if(windows[0]->CreateWindow("Main", 600, 400) == window::FAILURE)
-        return -1;
-
+    if(windows[0]->CreateWindow("Main", 600, 400) == window::FAILURE) return -1;
+    if(windows[1]->CreateWindow("Secondary", 300, 600) == window::FAILURE) return -1;
     windows[0]->SetBackground(0.2, 0.5, 0.3);
-    windows[1]->CreateWindow("Secondary", 300, 600);
     windows[1]->SetBackground(0.8, 0.1, 0.1);
 
 
@@ -67,10 +61,12 @@ int main()
     
     std::cout << "Number of windows " << windows.size() << std::endl;
 
-    glViewport(0, 0, WIDTH, HEIGHT);
 
     windows[0]->meshes.AddText("x, pos, 1.0f, color", glm::vec2(25.0f, 25.0f), 1.0f, glm::vec3(0.0f, 0.0f, 0.0f));
+    windows[1]->meshes.AddText("x, pos, 1.0f, color", glm::vec2(25.0f, 25.0f), 1.0f, glm::vec3(0.0f, 0.0f, 0.0f));
 
+
+    std::thread t(handleInput, std::ref(windows));
 
     while(!windows.empty())
     {
@@ -79,12 +75,7 @@ int main()
         for(int i = 0; i < windows.size(); ++i)
         {
             if(windows[i]->GetStatus() == window::OPENED)
-            {
-                struct window::mouse* m = windows[i]->GetMouse();
-                std::cout << m->mouseFirstMoved << '\t' << m->lastX << ' ' << m->lastY << '\t' << m->xChange << ' ' << m->yChange << std::endl;
-
                 windows[i]->Render();
-            }
             else
             {
                 delete windows[i];
@@ -94,11 +85,12 @@ int main()
         }
     
         std::cout << "------------------------------" << std::endl;
-
         std::chrono::duration<double> elapsed_seconds = std::chrono::steady_clock::now() - start;
 
         // std::cout << '\t' << 1/elapsed_seconds.count() << std::endl;
     }
+
+    t.join();
 
     glfwTerminate();
     return 0;
