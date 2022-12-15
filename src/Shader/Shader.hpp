3f2d84c6af4788c86ac85f5a301170c6ee3807d4
Bueno <matheusbartkev@gmail.com>
2022-12-15 09:17:58 -0300

    1.0

--- src/Shader/Shader.hpp
+++ src/Shader/Shader.hpp
@@ -1,3 +1,9 @@
+/*
+
+    ERRRO NO SHADER, NÃO ESTÁ FUNCIONANDO.
+
+*/
+
 #ifndef SHADER_HPP
 #define SHADER_HPP
 
@@ -47,53 +53,5 @@ namespace shader
         void AddShader(GLuint theProgram, const std::string& shaderCode, GLenum shaderType);
     };
 }
-
-
-/*
-#ifndef SHADER_HPP
-#define SHADER_HPP
-
-#include <string>
-#include <string.h>
-#include <fstream>
-
-#include <iostream>
-
-#include "../../glad/glad.h"
-#include <GLFW/glfw3.h>
-
-
-namespace shader
-{
-    class Shader
-    {
-        public:
-            Shader();
-
-            // Cria o `Program` para ser utilizado quando for realizado o desenho. 
-            void CreateFromString(const char* vertexCode, const char* fragmentCode);
-            void CreateFromFiles(const char* vertexLocation, const char* fragmentLocation);
-
-            std::string ReadFile(const char* fileLocation);
-
-            GLuint GetProjectionLocation();
-            GLuint GetModelLocation();
-            GLuint GetViewLocation();
-
-            void UseShader();
-            void ClearShader();
-
-            ~Shader();
-
-        private:
-            GLuint shaderID, uniformProjection, uniformModel, uniformView;
-
-            void CompileShader(const char* vertexCode, const char* fragmentCode);
-            void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType);
-    };
-}
-
-#endif
-*/
 
 #endif
